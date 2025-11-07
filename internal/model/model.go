1786baa80409a98e8bdd177d84dc135e3f75977e
whosensei <sukunagojo965@gmail.com>
2025-11-07 14:48:27 +0530

    new URL model and updated shorten response model

--- internal/model/model.go
+++ internal/model/model.go
@@ -13,20 +13,21 @@ type Api_response struct {
 }
 
 type URL struct {
-	Id         string    `json:"id"`
-	Long_url   string `json:"longurl"`
-	Short_url  string `json:"shorturl"`
-	Created_At string `json:"created_at"`
+	Id              string    `json:"id"`
+	Long_url        string    `json:"long_url"`
+	Short_code       string   `json:"short_code"`
+	Created_At      time.Time `json:"created_at"`
+	Expires_at      time.Time `json:"expires_at"`
+	Clicks          int64     `json:"clicks"`
+	Last_clicked_at time.Time `json:"last_clicked_at"`
 }
 
 type ShortenResponse struct {
-
-	Data string `json:"data"`  //the shorturl complete
-	Shortcode string `json:"shortcode"`
-	Created_at time.Time `json:"created_at"`
-	Expires_at time.Time `json:"expires_at,omitempty"`
-	Anonymous_Token string `json:"anonymous_token,omitempty"`
-	Remaining int `json:"remaining,omitempty"`
-	Permanent bool `json:"permanent"`
-
+	Data            string    `json:"data"` //the shorturl complete
+	Shortcode       string    `json:"short_code"`
+	Created_at      time.Time `json:"created_at"`
+	Expires_at      time.Time `json:"expires_at,omitempty"`
+	Anonymous_Token string    `json:"anonymous_token,omitempty"`
+	Remaining       int       `json:"remaining,omitempty"`
+	Permanent       bool      `json:"permanent"`
 }
