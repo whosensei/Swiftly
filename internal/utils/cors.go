ed2ba1c1b4cc08617cc167cfd3436da69a65092b
whosensei <sukunagojo965@gmail.com>
2025-11-12 17:16:49 +0530

    updated-cors to handle anonymous token

--- internal/utils/cors.go
+++ internal/utils/cors.go
@@ -1,25 +1,34 @@
 package utils
 
 import (
+	"log"
 	"net/http"
 	"os"
+
 	"github.com/rs/cors"
 )
 
-func CorsInit() *cors.Cors{
-	
-	var c *cors.Cors
+func CorsInit() *cors.Cors {
 
-	if os.Getenv("ENV") == "development" {
+	var c *cors.Cors
+	env := os.Getenv("ENV")
+	log.Printf("CORS initialized with ENV=%s", env)
+
+	if env == "development" {
 		c = cors.New(cors.Options{
 			AllowedOrigins: []string{
 				"http://localhost:3000",
 			},
-			AllowedMethods: []string{http.MethodGet,http.MethodPost},
+			AllowedMethods: []string{
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodOptions,
+			},
 			AllowCredentials: true,
 			AllowedHeaders: []string{
 				"Authorization",
 				"Content-Type",
+				"X-Anonymous-token",
 			},
 		})
 	} else {
@@ -28,15 +37,19 @@ func CorsInit() *cors.Cors{
 				"https://swftly.dev",
 				"https://www.swftly.dev",
 			},
-			AllowedMethods:   []string{http.MethodGet, http.MethodPost},
+			AllowedMethods: []string{
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodOptions,
+			},
 			AllowCredentials: true,
 			AllowedHeaders: []string{
 				"Authorization",
 				"Content-Type",
+				"X-Anonymous-token",
 			},
 		})
 	}
 
 	return c
 }
-
