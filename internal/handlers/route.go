0cb77394a73c93303f3a08b7f1a64a1113da5056
whosensei <sukunagojo965@gmail.com>
2025-11-13 12:46:05 +0530

    add : endpoints for retrieving anonymous and authenticated URLs

--- internal/handlers/route.go
+++ internal/handlers/route.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 )
 
+
 func RegisterRoute(mux *http.ServeMux, db *sql.DB) {
 
 	userHandler := &UserHandler{DB: db}
 
 	mux.HandleFunc("POST /shorten", userHandler.ShortenURL)
-	mux.HandleFunc("GET /{shorturl}", userHandler.Redirect_to_website)
-	mux.HandleFunc("GET /getall", userHandler.Gettallmaps)
+	mux.HandleFunc("GET /urls/anonymous", userHandler.Get_anon_urls)
+	mux.HandleFunc("GET /urls/authenticated", userHandler.Get_auth_urls)
+	// mux.HandleFunc("DELETE /urls/delete/{short_code}",userHandler.Delete_url)
+	mux.HandleFunc("GET /{short_code}", userHandler.Redirect_to_website)  // Keep this last!
 }
